235223033d9d8fe0c8af84dbec7ceb59f4efd98b
Kate.Friedman <Kate.Friedman@noaa.gov>
2023-05-05 16:24:51 +0000

    EPIC stack update for Hera
    Refs #80

--- modulefiles/hera.intel.lua
+++ modulefiles/hera.intel.lua
@@ -1,11 +1,11 @@
 help([[
 ]])
 
-prepend_path("MODULEPATH", "/scratch2/NCEPDEV/nwprod/hpc-stack/libs/hpc-stack/modulefiles/stack")
+prepend_path("MODULEPATH", "/scratch1/NCEPDEV/nems/role.epic/hpc-stack/libs/intel-2022.1.2/modulefiles/stack")
 
-local hpc_ver=os.getenv("hpc_ver") or "1.1.0"
-local hpc_intel_ver=os.getenv("hpc_intel_ver") or "18.0.5.274"
-local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2018.0.4"
+local hpc_ver=os.getenv("hpc_ver") or "1.2.0"
+local hpc_intel_ver=os.getenv("hpc_intel_ver") or "2022.1.2"
+local hpc_impi_ver=os.getenv("hpc_impi_ver") or "2022.1.2"
 local cmake_ver=os.getenv("cmake_ver") or "3.20.1"
 
 load(pathJoin("hpc", hpc_ver))
